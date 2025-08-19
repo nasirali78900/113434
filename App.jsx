c91371caec1dd771b943aa74977668583a8a1220
nasirali78900 <Yt.automate2025@gmail.com>
2025-08-19 11:14:29 +0500

    src

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-50">
+      <h1 className="text-4xl font-bold text-pink-600">Wedding Invitation</h1>
+      <p className="mt-4 text-lg text-gray-700">
+        You are cordially invited to our wedding celebration!
+      </p>
+    </div>
+  )
+}
